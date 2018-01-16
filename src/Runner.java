a0f7de3f9df95795f07a8836859488ca00216c27
Arthur Tran <tran.art13@gmail.com>
2018-01-16 15:07:20 -0800

    Finished up the JFrame variables

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,6 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		
+		Window win = new Window(1280, 720, "Mini Golf");
 	}
 }
---
+++ src/Window.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Window {
+
+	public Window(int width, int height, String title) {
+		JFrame frame = new JFrame();
+		
+		frame.setVisible(true);
+		frame.setSize(width, height);
+		frame.setTitle(title);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
