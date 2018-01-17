ad752c6e53951c123bd8faf87640a33510798210
Arthur Tran <tran.art13@gmail.com>
2018-01-16 18:28:40 -0800

    Game class proposal

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,22 @@
 package game;
 
-public class Game {
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements ActionListener {
 
 	public Game() {
+		
+	}
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
 		
 	}
 }
