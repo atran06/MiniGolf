c7e1bf8f95a25dae904acb63e8abc31e0cb6fd03
Arthur Tran <tran.art13@gmail.com>
2018-01-17 13:09:33 -0800

    If you wanted to make a game loop

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,24 +2,20 @@ package game;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
-public class Game extends JPanel implements ActionListener {
-
-	Timer timer = new Timer(5, this);
+public class Game extends JPanel implements Runnable {
 	
 	public Game() {
-		timer.start();
+		
 	}
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 	}
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void run() {
+		// TODO Auto-generated method stub
 		
 	}
 }
